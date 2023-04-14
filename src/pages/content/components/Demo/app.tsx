372822b5ed62a98ac314359aa8afbee796b353c2
Artiu <oborskiartur123@gmail.com>
2023-04-14 08:57:34 +0200

    Remove unnecesary files

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,4 +1,3 @@
-import logo from "@src/assets/img/logo.svg";
 import "@src/styles/index.css";
 import styles from "./App.module.css";
 
@@ -7,11 +6,6 @@ const App = () => {
     <div class="fixed right-5 top-20 z-[2000] w-80 rounded-xl bg-white">
       <div class={styles.App}>
         <header class={styles.header}>
-          <img
-            src={chrome.runtime.getURL(logo)}
-            class={styles.logo}
-            alt="logo"
-          />
           <p class="flex flex-wrap font-bold">
             Edit <code>src/pages/content/index.tsx</code> and save to reload.
           </p>
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,4 +1,3 @@
-import logo from "@assets/img/logo.svg";
 import "@src/styles/index.css";
 import styles from "./Popup.module.css";
 
@@ -6,7 +5,6 @@ const Popup = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
         <p class="font-bold">
           Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
         </p>
