3fcc41c02bfd6430841ab90b8970aad959e0475a
Artiu <71273256+Artiu@users.noreply.github.com>
2023-07-24 03:31:38 +0200

    Add more icons

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,38 +3,41 @@ import packageJson from "../package.json";
 
 // Convert from Semver (example: 0.1.0-beta6)
 const [major, minor, patch, label = "0"] = packageJson.version
-    // can only contain digits, dots, or dash
-    .replace(/[^\d.-]+/g, "")
-    // split into version parts
-    .split(/[.-]/);
+	// can only contain digits, dots, or dash
+	.replace(/[^\d.-]+/g, "")
+	// split into version parts
+	.split(/[.-]/);
 
 const manifest = defineManifest(async () => ({
-    manifest_version: 3,
-    name: packageJson.displayName ?? packageJson.name,
-    version: `${major}.${minor}.${patch}.${label}`,
-    description: packageJson.description,
-    background: { service_worker: "src/pages/background/index.ts" },
-    action: {
-        default_popup: "src/pages/popup/index.html",
-        default_icon: "icons/logo.png",
-    },
-    icons: {
-        "128": "icons/logo.png",
-    },
-    content_scripts: [
-        {
-            matches: ["https://*.youtube.com/*"],
-            js: ["src/pages/content/index.ts"],
-        },
-    ],
-    web_accessible_resources: [
-        {
-            resources: ["assets/js/*.js", "assets/css/*.css", "assets/img/*"],
-            matches: ["https://*.youtube.com/*"],
-        },
-    ],
-    permissions: ["tabs", "clipboardWrite", "scripting"],
-    host_permissions: ["https://*.youtube.com/*"],
+	manifest_version: 3,
+	name: packageJson.displayName ?? packageJson.name,
+	version: `${major}.${minor}.${patch}.${label}`,
+	description: packageJson.description,
+	background: { service_worker: "src/pages/background/index.ts" },
+	action: {
+		default_popup: "src/pages/popup/index.html",
+		default_icon: "icons/128.png",
+	},
+	icons: {
+		"16": "icons/16.png",
+		"32": "icons/32.png",
+		"48": "icons/48.png",
+		"128": "icons/128.png",
+	},
+	content_scripts: [
+		{
+			matches: ["https://*.youtube.com/*"],
+			js: ["src/pages/content/index.ts"],
+		},
+	],
+	web_accessible_resources: [
+		{
+			resources: ["assets/js/*.js", "assets/css/*.css", "assets/img/*"],
+			matches: ["https://*.youtube.com/*"],
+		},
+	],
+	permissions: ["tabs", "clipboardWrite", "scripting"],
+	host_permissions: ["https://*.youtube.com/*"],
 }));
 
 export default manifest;
