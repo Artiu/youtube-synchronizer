63291f5aaf6242810efed96fb3155338785e289e
Artiu <oborskiartur123@gmail.com>
2026-05-14 01:55:53 +0200

    Remove kofi

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -26,16 +26,6 @@ const Popup = () => {
 						</button>
 					</Show>
 				</div>
-				<div class="mt-8 w-fit mx-auto">
-					<a href="https://ko-fi.com/Z8Z0KABI5" target="_blank">
-						<img
-							height="36"
-							style="border:0px;height:36px;"
-							src="https://storage.ko-fi.com/cdn/kofi1.png?v=3"
-							alt="Buy Me a Coffee at ko-fi.com"
-						/>
-					</a>
-				</div>
 			</div>
 		</div>
 	);
